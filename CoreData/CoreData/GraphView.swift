1b1d8b9f900ba7f321c96f650cfc005d53f9dcca
Emmet Susslin <emmet@Emmets-MacBook-Pro.local>
2017-07-04 19:24:12 -0700

    lolol

---
+++ CoreData/CoreData/GraphView.swift
@@ -0,0 +1,43 @@
+//
+//  GraphView.swift
+//  CoreData
+//
+//  Created by Emmet Susslin on 7/4/17.
+//  Copyright © 2017 Emmet Susslin. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class GraphView: UIView {
+    
+    //1 - the properties for the gradient
+    @IBInspectable var startColor: UIColor = UIColor.red
+    @IBInspectable var endColor: UIColor = UIColor.green
+    
+    override func draw(_ rect: CGRect) {
+        
+        
+        //2 - get the current context
+        let context = UIGraphicsGetCurrentContext()
+        let colors = [startColor.cgColor, endColor.cgColor]
+        
+        //3 - set up the color space
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        //4 - set up the color stops
+        let colorLocations:[CGFloat] = [0.0, 1.0]
+        
+        //5 - create the gradient
+        let gradient = CGGradient(colorsSpace: colorSpace,
+                                  colors: colors as CFArray,
+                                  locations: colorLocations)
+        
+        //6 - draw the gradient
+        var startPoint = CGPoint.zero
+        var endPoint = CGPoint(x:0, y:self.bounds.height)
+        context!.drawLinearGradient(gradient!,
+                                    start: startPoint,
+                                    end: endPoint,
+                                    options: CGGradientDrawingOptions(rawValue: 0))
+    }
+}
